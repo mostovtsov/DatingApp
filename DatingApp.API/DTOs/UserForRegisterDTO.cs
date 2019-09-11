d5c1180dd799713c3522faf190a717fff643d1c5
Alexander Mostovtsov <mostovtsov@gmail.com>
2019-09-11 11:20:03 +0300

    typo fix

--- DatingApp.API/DTOs/UserForRegisterDTO.cs
+++ DatingApp.API/DTOs/UserForRegisterDTO.cs
@@ -7,7 +7,7 @@ namespace DatingApp.API.DTOs
         [Required]
         public string Username { get; set;}
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 an 8 charachters")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 an 8 characters")]
         public string Password { get; set;}
     }
 }
